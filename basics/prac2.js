f896de081213e505fdaf8afce497a23111bc2d7a
Vanshita1725 <144545424+Vanshita1725@users.noreply.github.com>
2025-03-25 09:30:32 +0000

    add file

---
+++ basics/prac2.js
@@ -0,0 +1,17 @@
+const accountid =123456
+let accoutemail = "vanshitarathod1725@gmail.com"
+var accountpass ="12345"
+accountcity ="ahemedabad"
+// accountid = 2 const is not change value
+console.log(accountid);
+console.table([ accountid , accoutemail,accountpass,accountcity])
+
+// data types
+"use strict";
+
+console.log(3+3);
+console.log("vanshita");
+
+console.log(typeof undefined);
+console.log(typeof null);
+
